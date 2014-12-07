1c505f8e8095ecba7283648e151b5390e29d7f25
peter <a@a.com>
2014-12-07 18:26:13 +0800

    第四章，第五章6题

--- CPlusPrimerPlus/CPlusPrimerPlus.cpp
+++ CPlusPrimerPlus/CPlusPrimerPlus.cpp
@@ -2,9 +2,24 @@
 //
 
 #include "stdafx.h"
-#include "iostream"
+#include <iostream>
+#include <string>
+#include <array>
 using namespace std;
 
+struct CandyBar
+{
+	string Brand;
+	double Weight;
+	int Calorie;
+};
+
+struct Pizza
+{
+	string Corp;
+	double Diameter;
+	double Weight;
+};
 
 void Fun271();
 void Fun272();
@@ -22,9 +37,23 @@ void Func374();
 void Func375();
 void Func376();
 void Func377();
+void Func4131();
+void Func4133();
+void Func4135();
+void Func4136();
+void Func4137();
+void Func4138();
+void Func4139();
+void Func41310();
+void Func591();
+void Func592();
+void Func593();
+void Func594();
+void Func595();
+void Func596();
 int _tmain(int argc, _TCHAR* argv[])
 {
-	Func377();
+	Func596();
 	return 0;
 }
 
@@ -203,4 +232,199 @@ void Func377()
 	double result = mile / gallon;
 
 	cout << litre << " l/km = " << result << " mpg.";
+}
+
+void Func4131()
+{
+	string firstname,lastname;
+	char letter;
+	int age;
+	cout << "What is your first name?";
+	getline(cin, firstname);
+	cout << "What is your last name?";
+	cin >> lastname;
+	cout << "What letter grade do you deserve?";
+	cin >> letter;
+	cout << "What is your age?";
+	cin >> age;
+
+	cout << "Name:" << lastname << "," << firstname << endl << "Grade:" << (char)(letter + 1) << endl << "Age:" << age;
+}
+void Func4133()
+{
+	//const int size = 20;
+	//char firstname[size];
+	//char lastname[size];
+	//char fullname[size+2];
+	string firstname, lastname,fullname;
+	cout << "Enter your first name:";
+	cin >> firstname;
+	cout << "Enter your last name:";
+	cin >> lastname;
+	
+	fullname = lastname + ", " + firstname;
+	//strcpy_s(fullname, lastname);
+	//strcat_s(fullname, ", ");
+	//strcat_s(fullname, firstname);
+
+	cout << "Here's the infomation in a single string: " << fullname;
+}
+void Func4135()
+{
+	CandyBar snack{ "Mocha Munch", 2.3, 350 };
+	cout << "Brand:" << snack.Brand << ",Weight:" << snack.Weight << ",Calorie:" << snack.Calorie;
+}
+void Func4136()
+{
+	CandyBar snack[]{ {"Mocha Munch1", 2.3, 350}, { "Mocha Munch2", 2.4, 351 }, { "Mocha Munch3", 2.5, 352 } };
+	cout << "snack[0] Brand:" << snack[0].Brand << ",Weight:" << snack[0].Weight << ",Calorie:" << snack[0].Calorie << endl;
+	cout << "snack[1] Brand:" << snack[1].Brand << ",Weight:" << snack[1].Weight << ",Calorie:" << snack[1].Calorie << endl;
+	cout << "snack[2] Brand:" << snack[2].Brand << ",Weight:" << snack[2].Weight << ",Calorie:" << snack[2].Calorie << endl;
+}
+void Func4137()
+{
+	Pizza pizza;
+	cout << "Enter Pizza Corp:";
+	getline(cin, pizza.Corp);
+	cout << "Enter Pizza Diameter:";
+	cin >> pizza.Diameter;
+	cout << "Enter Pizza Weight:";
+	cin >> pizza.Weight;
+
+	cout << "pizza, Corp:" << pizza.Corp << ",Diameter:" << pizza.Diameter << ",Weight:" << pizza.Weight;
+}
+void Func4138()
+{
+	Pizza * pizza = new Pizza();
+
+	cout << "Enter Pizza Diameter:";
+	cin >> pizza->Diameter;
+	cin.get();
+	cout << "Enter Pizza Corp:";
+	getline(cin, pizza->Corp);
+	cout << "Enter Pizza Weight:";
+	cin >> pizza->Weight;
+
+	cout << "pizza, Corp:" << pizza->Corp << ",Diameter:" << pizza->Diameter << ",Weight:" << pizza->Weight;
+	delete pizza;
+}
+void Func4139()
+{
+	CandyBar * snack = new CandyBar[3]{ { "Mocha Munch1", 2.3, 350 }, { "Mocha Munch2", 2.4, 351 }, { "Mocha Munch3", 2.5, 352 } };
+	cout << "snack[0] Brand:" << snack[0].Brand << ",Weight:" << snack[0].Weight << ",Calorie:" << snack[0].Calorie << endl;
+	cout << "snack[1] Brand:" << snack[1].Brand << ",Weight:" << snack[1].Weight << ",Calorie:" << snack[1].Calorie << endl;
+	cout << "snack[2] Brand:" << snack[2].Brand << ",Weight:" << snack[2].Weight << ",Calorie:" << snack[2].Calorie << endl;
+	delete [] snack;
+}
+void Func41310()
+{
+	array<double, 3> scores;
+
+	cin >> scores[0] >> scores[1] >> scores[2];
+
+	cout << "1." << scores[0] << " ,2." << scores[1] << " ,3." << scores[2] << endl;
+
+	double avg = (scores[0] + scores[1] + scores[2]) / 3;
+	cout << "avg:" << avg;
+}
+
+void Func591()
+{
+	int a, b, sum = 0;
+	cout << "Enter two numbers:";
+	cin >> a >> b;
+
+	for (int i = a; i <= b; i++)
+	{
+		sum += i;
+	}
+	cout << sum;
+}
+void Func592()
+{
+	const int arsize = 101;
+	array<long double, arsize> factorials;
+	factorials[1] = factorials[0] = 1LL;
+	for (int i = 2; i < arsize; i++)
+	{
+		factorials[i] = i*factorials[i - 1];
+	}
+	for (int i = 0; i < arsize; i++)
+	{
+		cout << i << "! = " << factorials[i] << endl;
+	}
+}
+void Func593()
+{
+	int sum = 0;
+	int n;
+	while (cin >> n && n!=0)
+	{
+		sum += n;
+		cout << "sum:" << sum << endl;
+
+	}
+}
+void Func594()
+{
+	double daphne = 100, celo = 100, drate = 0.1, crate = 0.05, dsum = 100, csum = 100;
+
+	
+	int year = 1;
+	while (dsum>=csum)
+	{
+		
+		dsum += drate * daphne;
+		csum += csum * crate;
+		cout << "year:" << year << ", Daphne:" << dsum << ", Celo:" << csum << endl;
+		year++;
+		
+	}
+}
+void Func595()
+{
+	const int size = 12;
+	string months[size]{ "1.", "2.", "3.", "4.", "5.", "6.", "7.", "8.", "9.", "10.", "11.", "12." };
+	int sales[size]{0};
+	for (int i = 0; i < size; i++)
+	{
+		cout << months[i] << ":";
+		cin >> sales[i];
+	}
+
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum += sales[i];
+	}
+	cout << sum;
+}
+void Func596()
+{
+	const int size = 12;
+	const int years = 3;
+	string months[size]{ "1.", "2.", "3.", "4.", "5.", "6.", "7.", "8.", "9.", "10.", "11.", "12." };
+	int sales[years][size];
+	for (int i = 0; i < years; i++)
+	{
+		for (int j = 0; j < size; j++)
+		{
+			cout << "year:" << i + 1 << ",month:" << months[j];
+			cin >> sales[i][j];
+		}
+	}
+
+
+	int yearsum = 0, total = 0;
+	for (int i = 0; i < years; i++)
+	{
+		yearsum = 0;
+		for (int j = 0; j < size; j++)
+		{
+			yearsum += sales[i][j];
+		}
+		cout << "year" << (i + 1) << ":" << yearsum << endl;
+		total += yearsum;
+	}
+	cout << "total:" << total << endl;
 }
